7ff6454dc983653e8bb802e4d41ce89663143055
Krissy Macdonald <krissymacdonald5@gmail.com>
2023-04-18 15:50:22 -0400

    brand statement added

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,6 +2,11 @@
 
 export default function AboutMe() {
     return (
-        <div> AboutMe </div>
+        <>
+            <h4>Image</h4>
+            <h3>Hi, my name is Krissy Macdonald</h3>
+            <p>I am an independent, hard-working developer that elevates my skills with each project I take on. My attention to detail, communication skills and problem solving ability enhance and support my creativity in every project I lead, or support. I possess the highest standards of ethics, compassion, integrity and success in all that I do. My specialty skills include: white glove customer service, direct and indirect sales, internal process and workflow design, process implementation, training and, as required, process redesign and re-implementation. I’m ready to step into my future and continue to build my brand as an efficient and effective developer that will further boost productivity, profits and positive brand recognition.
+                </p>
+        </>
     )
 }
