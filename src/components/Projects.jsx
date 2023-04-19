15ab101494f12c9d1466bbade11858770c40c993
Krissy Macdonald <krissymacdonald5@gmail.com>
2023-04-19 17:25:03 -0400

    contact form removed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,14 @@
+import { Link } from 'react-router-dom'
 
 
 export default function Projects() {
     return (
-        <div> Projects </div>
+        <div> 
+            <Link to='https://booksie.netlify.app/'>
+                <h4> booksie! </h4>
+            </Link>
+        </div>
+
     )
 }
 
